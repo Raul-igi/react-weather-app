9e16d0dab44389f518ae69a3b081d35f9fbc10ff
Raul-igi <igiranezaraul@gmail.com>
2024-02-25 17:12:35 +0200

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "401f79fbb2msh779af0004ce4566p14957bjsnbab555f012e3",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "bb9d856c37dbade632e7c684894600c3";
